2c1692d0a8fda25c8446b23b14520236461265c7
Angel Zamora <angeljza0612@gmail.com>
2026-06-26 12:04:45 -0600

    Corrección en ancho de línea de separación

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,10 +11,10 @@ export default function Home() {
       <Navbar />
       <main className="flex-1 w-full max-w-6xl mx-auto px-4 pb-4">
         <Hero />
-        <div className="w-100 md:w-300 h-1.5 mx-auto my-8 bg-gradient-to-r from-transparent via-white/40 to-transparent" />
+        <div className="w-4/5 md:w-300 h-1.5 mx-auto my-8 bg-gradient-to-r from-transparent via-white/40 to-transparent" />
         <Bio />
         <TechStack />
-        <div className="w-100 md:w-300 h-1.5 mx-auto my-8 bg-gradient-to-r from-transparent via-white/40 to-transparent" />
+        <div className="w-4/5 md:w-300 h-1.5 mx-auto my-8 bg-gradient-to-r from-transparent via-white/40 to-transparent" />
         <NavCards />
       </main>
       <Footer />
